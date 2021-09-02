e6d41773b6f1d01304b19483d02b4bed568e4fb6
Gergely Laborci <gergely@laborci.hu>
2021-09-02 13:43:06 +0200

    get Roles method added

--- plugin-carbon/AuthorizableTrait.php
+++ plugin-carbon/AuthorizableTrait.php
@@ -14,4 +14,14 @@ trait AuthorizableTrait {
 		return false;
 	}
 
+	public function getRoles(): array {
+		$plugin = Authorizable::fetch(static::model());
+		$roles = [];
+		foreach (is_array($this->{$plugin->field}) ? $this->{$plugin->field} : [$this->{$plugin->field}] as $group) {
+			if (array_key_exists($group, static::GROUPS) && is_array(static::GROUPS[$group])){
+				$roles = array_merge($roles, static::GROUPS[$group]);
+			}
+		}
+		return array_unique($roles);
+	}
 }
